783e37d508dd2cb486b5037c402581e0b7e4fd9a
Sendarg Lee <Sendarg@users.noreply.github.com>
2017-06-21 17:49:07 +0800

    fix label

--- diff_excel.py
+++ diff_excel.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # version 1.0 update by le @ 2017.6.10 for qxq
-# version 1.0 update by le @ 2017.6.18 for qxq, support group compare
+# version 2.0 update by le @ 2017.6.18 for qxq, support group compare
+# version 2.1 update by le @ 2017.6.21 for qxq, fix mark label
 
 import os, glob
 from openpyxl import Workbook, load_workbook
@@ -10,6 +11,7 @@ from argparse import RawTextHelpFormatter
 
 old_label = u""
 new_label = u""
+both_label = u""
 
 
 def get_unique_key_seqs(Sheet, Keys):
@@ -39,12 +41,12 @@ def get_num_key(Keys):
 	return Key
 
 
-def mark_diff1(Workbook, Sheet, Indexs, MarkLabel, Basefilename):
+def mark_index_label(Workbook, Sheet, Indexs, Basefilename):
 	c = Sheet.max_column
 	
 	Sheet.cell(row=1, column=c + 1).value = u"Changes"
 	for i in Indexs:
-		Sheet.cell(row=i + 1, column=c + 1).value = MarkLabel
+		Sheet.cell(row=i[0] + 1, column=c + 1).value = i[1]
 	
 	name = Basefilename.decode('utf-8').split("/")[-1]
 	print "== OK:\t[%s]\t[%s]" % (name, Sheet.title)
@@ -69,7 +71,7 @@ def single_unique_workbook(SheetName, OnlyData, MarkLabel, Basefilename):
 	ws = wb.active
 	ws.title = SheetName
 	
-	for d in OnlyData:
+	for d in set(OnlyData):  # unique
 		line = d.split("^^")
 		ws.append(line)
 	name = Basefilename.decode('utf-8').split("/")[-1]
@@ -77,34 +79,30 @@ def single_unique_workbook(SheetName, OnlyData, MarkLabel, Basefilename):
 	wb.save("single-unique-%s-%s" % (MarkLabel, name))
 
 
-def get_sub_index_index(Big_2LevelList, Sub_1LevelList, GroupIndex):
+def get_sub_index_index(Big_2LevelList, Sub_1LevelList):
 	# get all index form K1s seq not just first one
 	index_a = [[[i, j] for i, x in enumerate(Big_2LevelList) for j, y in enumerate(Big_2LevelList[i]) if y == o] for o
 	           in Sub_1LevelList]
 	index_b = [j for i in index_a for j in i]
+	return index_b
+
+
+def group_list_by_index(index, GroupIndex):
 	# group 2 single list
 	## method1,lambda ?? what mean?
-	index_c = [[i for i in index_b if i[GroupIndex] == u] for u in set(map(lambda x: x[GroupIndex], index_b))]
+	index_c = [[i for i in index if i[GroupIndex] == u] for u in set(map(lambda x: x[GroupIndex], index))]
 	# ## method2 , sort+group
 	# index1b.sort(key=itemgetter(0))
 	# index1c=[[i for i in g] for k,g in groupby(index1b,key=itemgetter(0))]
 	return index_c
 
 
-def get_sub_index_value(Big_2LevelList, Sub_1LevelList, GroupIndex):
+def get_sub_index_value(Big_2LevelList, Sub_1LevelList):
 	# get all index form K1s seq not just first one
 	index_a = [[[i, o] for i, x in enumerate(Big_2LevelList) for j, y in enumerate(Big_2LevelList[i]) if y == o] for o
 	           in Sub_1LevelList]
 	index_b = [j for i in index_a for j in i]
-	# group 2 single list
-	## method1,lambda ?? what mean?
-	index_c = [[i for i in index_b if i[GroupIndex] == u] for u in set(map(lambda x: x[GroupIndex], index_b))]
-	# ## method2 , sort+group
-	# from itertools import groupby
-	# from operator import itemgetter
-	# index1b.sort(key=itemgetter(0))
-	# index1c=[[i for i in g] for k,g in groupby(index1b,key=itemgetter(0))]
-	return index_c
+	return index_b
 
 
 def diffPlus(Old_Files, New_Files, Sheet_Index_From_Old, Sheet_Index_From_New, Key_From_Old, Key_From_New, Single_File,
@@ -112,33 +110,40 @@ def diffPlus(Old_Files, New_Files, Sheet_Index_From_Old, Sheet_Index_From_New, K
 	W1s = [load_workbook(filename=old) for old in Old_Files]
 	S1s = [w1.worksheets[Sheet_Index_From_Old] for w1 in W1s]
 	K1s = [get_unique_key_seqs(s1, Key_From_Old) for s1 in S1s]
-	K1 = [j for i in K1s for j in i]
+	K1 = [j for i in K1s for j in i[1:]]  # 去掉每一个标题
 	
 	W2s = [load_workbook(filename=new) for new in New_Files]
 	S2s = [w2.worksheets[Sheet_Index_From_New] for w2 in W2s]
 	K2s = [get_unique_key_seqs(s2, Key_From_New) for s2 in S2s]
-	K2 = [j for i in K2s for j in i]
+	K2 = [j for i in K2s for j in i[1:]]
 	
 	only1 = set(K1) - set(K2)
 	only2 = set(K2) - set(K1)
-	# both = (set(K1s) & set(K2s)) - set(K1s[0:1]) - set(K2s[0:1])  # 去掉标题
+	both = set(K1) & set(K2)
 	
 	## only old
-	index1 = get_sub_index_index(K1s, only1, 0)
-	index1o = get_sub_index_value(K1s, only1, 0)
+	index1 = get_sub_index_index(K1s, only1)
+	[[i.append(old_label)] for i in index1]
+	both1 = get_sub_index_index(K1s, both)
+	[[b.append(both_label)] for b in both1]
+	index1_value = get_sub_index_value(K1s, only1)
 	
 	## only new
-	index2 = get_sub_index_index(K2s, only2, 0)
-	index2o = get_sub_index_value(K2s, only2, 0)
+	index2 = get_sub_index_index(K2s, only2)
+	for i in index2:i.append(new_label)
+	both2 = get_sub_index_index(K2s, both)
+	[[b.append(both_label)] for b in both2]
+	index2_value = get_sub_index_value(K2s, only2)
 	
 	if not Single_File:
-		## fucking here
 		# clean
 		[os.remove(f) for f in glob.glob("mark-*")]
-		[mark_diff1(W1s[i[0][0]], S1s[i[0][0]], [j[1] for j in i], old_label, Old_Files[i[0][0]].name)
-		 for i in index1]
-		[mark_diff1(W2s[i[0][0]], S2s[i[0][0]], [j[1] for j in i], new_label, New_Files[i[0][0]].name)
-		 for i in index2]
+		index1_both1 = group_list_by_index(index1 + both1, 0)
+		[mark_index_label(W1s[i[0][0]], S1s[i[0][0]], [j[1:3] for j in i], Old_Files[i[0][0]].name)
+		 for i in index1_both1]
+		index2_both2 = group_list_by_index(index2 + both2, 0)
+		[mark_index_label(W2s[i[0][0]], S2s[i[0][0]], [j[1:3] for j in i], New_Files[i[0][0]].name)
+		 for i in index2_both2]
 	
 	else:
 		if not Only_Unique_Key:
@@ -146,14 +151,14 @@ def diffPlus(Old_Files, New_Files, Sheet_Index_From_Old, Sheet_Index_From_New, K
 			# disable only old, not needed and much write operate
 			# [single_workbook(S1s[i[0][0]], [j[1] for j in i], Key_From_Old, old_label, Old_Files[i[0][0]].name) for i in index1c]
 			[single_workbook(S2s[i[0][0]], [j[1] for j in i], Key_From_New, new_label, New_Files[i[0][0]].name) for i in
-			 index2]
+			 group_list_by_index(index2, 0)]
 		# single_workbook(s2, both_index2, Key_From_New, u"both", New_File)
 		else:
 			[os.remove(f) for f in glob.glob("single-unique-*")]
 			[single_unique_workbook(S1s[i[0][0]].title, [j[1] for j in i], old_label, Old_Files[i[0][0]].name) for i in
-			 index1o]
+			 group_list_by_index(index1_value, 0)]
 			[single_unique_workbook(S2s[i[0][0]].title, [j[1] for j in i], new_label, New_Files[i[0][0]].name) for i in
-			 index2o]
+			 group_list_by_index(index2_value, 0)]
 	print "== Done"
 
 
@@ -186,8 +191,8 @@ def diff1(Old_File, New_File, Sheet_Index_From_Old, Sheet_Index_From_New, Key_Fr
 	# both_index2 = [j for i in both_index2 for j in i]
 	
 	if not Single_File:
-		mark_diff1(w1, s1, index1, old_label, Old_File.name)
-		mark_diff1(w2, s2, index2, new_label, New_File.name)
+		mark_index_label(w1, s1, index1, old_label, Old_File.name)
+		mark_index_label(w2, s2, index2, new_label, New_File.name)
 	else:
 		if not Only_Unique_Key:
 			single_workbook(s1, index1, Key_From_Old, old_label, Old_File.name)
@@ -220,19 +225,22 @@ if __name__ == '__main__':
 	                    help="unique Key Column from older excel,Simple: A or A+C or B+D+G")
 	parser.add_argument("-k2", dest="Key_From_New", required=True,
 	                    help="unique Key Column from newer excel,Simple: A or B+C or D+A+F")
+	parser.add_argument("-l0", dest="Mark_Both_Label", default=u"both",
+	                    help="Special a word to mark both line in all files as Label, default is \"both\"")
 	parser.add_argument("-l1", dest="Mark_Old_Label", default=u"old",
-	                    help="Special a word to mark different line in older file as Label, default is \"old\"")
-	parser.add_argument("-l2", dest="Mark_New_Label", default=u"新增",
-	                    help="Special a word to mark different line in newer file as Label, default is \"新增\"")
+	                    help="Special a word to mark different line in older files as Label, default is \"old\"")
+	parser.add_argument("-l2", dest="Mark_New_Label", default=u"new",
+	                    help="Special a word to mark different line in newer files as Label, default is \"new\"")
 	parser.add_argument("-s", dest="Single_File", action="store_true", default=False,
 	                    help="output different lines to 2 single excel file")
 	parser.add_argument("-u", dest="Only_Unique_Key", action="store_true", default=False,
 	                    help="only output unique different key column data to 2 single excel file, use only with -s option")
-	parser.add_argument('-v', action='version', version='%(prog)s 2.0')
+	parser.add_argument('-v', action='version', version='%(prog)s 2.1')
 	args = parser.parse_args()
 	
 	old_label = args.Mark_Old_Label
 	new_label = args.Mark_New_Label
+	both_label = args.Mark_Both_Label
 	
 	diffPlus(args.Old_Files, args.New_Files, args.Sheet_Index_From_Old, args.Sheet_Index_From_New, args.Key_From_Old,
 	         args.Key_From_New, args.Single_File, args.Only_Unique_Key)
